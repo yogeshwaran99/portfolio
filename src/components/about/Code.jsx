d90e4c1c81b1d9a244c21ac3c7dd255807049bfe
yogeshwaran99 <itsyogeshwaran@gmail.com>
2024-10-26 13:52:10 +0530

    slide correction

--- src/components/about/Code.jsx
+++ src/components/about/Code.jsx
@@ -5,6 +5,7 @@ import Hackerrank from '../../assets/hackerrank.png';
 import Leetcode from '../../assets/leetcode.png';
 import Geeksforgeeks from '../../assets/geeksforgeeks.png';
 import Codeforces from '../../assets/codeforces.png';
+import { useMediaQuery } from 'react-responsive';
 
 import './code.css';
 
@@ -12,13 +13,16 @@ import 'swiper/css';
 import 'swiper/css/pagination';
 
 const Code = () => {
+  const isMobile = useMediaQuery({ query: '(max-width: 600px)' });
+  const isDesktop = useMediaQuery({ query: '(min-width: 1024px)' });
+
   const code = [
     {
       id: 1,
       img: Hackerrank,      
       platform: 'HackerRank',
       problemssolved: '142',
-      star:'5 (Java)',
+      star: '5 (Java)',
       link: 'https://www.hackerrank.com/profile/yogeshwaran99',      
     },
     {
@@ -53,43 +57,32 @@ const Code = () => {
       <Swiper 
         className="container code__container"
         modules={[Pagination]}
-        spaceBetween={30} 
-        slidesPerView={1}
+        spaceBetween={isMobile ? 40 : (isDesktop ? 20 : 30)} 
+        slidesPerView={isMobile ? 1 : (isDesktop ? 4 : 2)}
         pagination={{ clickable: true }}
-         loop={true} 
-        breakpoints={{
-          1024: {
-            slidesPerView: 4,
-            spaceBetween: 20,
-          },
-          600: {
-            slidesPerView: 1,
-            spaceBetween: 40,
-          },
-        }}
+        loop={isMobile} 
       >
         {code.map((coding) => (
-         <SwiperSlide className="code" key={coding.id}>
-         <div className="code__img-name">
-           <div className="code__img">
-             <img src={coding.img} alt={coding.platform} />
-           </div>
-           <h5 className="code__name">{coding.platform}</h5>
-         </div>
-         
-         <h4 className="code_name">
-           {coding.id === 1 ? `Stars: ${coding.star}` : `Contest Rating: ${coding.rating}`}
-         </h4>
-         <h4 className="code_name">Problems Solved: {coding.problemssolved}</h4>
-         <a href={coding.link} target="_blank" className="btn" rel="noreferrer">
-           Visit
-         </a>
-       </SwiperSlide>
-       
+          <SwiperSlide className="code" key={coding.id}>
+            <div className="code__img-name">
+              <div className="code__img">
+                <img src={coding.img} alt={coding.platform} />
+              </div>
+              <h5 className="code__name">{coding.platform}</h5>
+            </div>
+            
+            <h4 className="code_name">
+              {coding.id === 1 ? `Stars: ${coding.star}` : `Contest Rating: ${coding.rating}`}
+            </h4>
+            <h4 className="code_name">Problems Solved: {coding.problemssolved}</h4>
+            <a href={coding.link} target="_blank" className="btn" rel="noreferrer">
+              Visit
+            </a>
+          </SwiperSlide>
         ))}
       </Swiper>
     </section>
-  )
+  );
 }
 
 export default Code;
