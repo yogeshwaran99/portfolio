6765a47574ba9ef79537a4e9ef9adbc70e5d0ee9
yogeshwaran99 <itsyogeshwaran@gmail.com>
2025-04-26 21:45:30 +0530

    skills object

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -2,106 +2,63 @@ import React from 'react';
 import { BsFillPatchCheckFill } from 'react-icons/bs';
 import './skills.css';
 
+const skillsData = {
+  skills: [
+    'Java',
+    'Springboot',
+    'Javascript',
+    'APIs',
+    'Python',
+    'HTML',
+    'C/C++',
+    'CSS',
+    'SQL',
+    'React JS'
+  ],
+  tools: [
+    'Eclipse',
+    'IntelliJ IDEA',
+    'VS Code',
+    'Postman',
+    'Git & GitHub',
+    'Docker',
+    'Linux',
+    'Vim',
+    'Bash',
+    'Android Studio'
+  ]
+};
+
 const Skills = () => {
+  const skill = (items) => {
+    return items.map((item, index) => (
+      <article key={index} className="skills__details">
+        <BsFillPatchCheckFill className="skills__details-icon" />
+        <p>{item}</p>
+      </article>
+    ));
+  };
+
   return (
-    <section id="skills"> 
+    <section id="skills">
       <h5>Explore My</h5>
       <h2>Skills</h2>
-      <div className="container skills__container"> 
-        <div className="skills__tech"> 
-        <h3>Skills</h3>
+      <div className="container skills__container">
+        <div className="skills__tech">
+          <h3>Skills</h3>
           <div className="skills__content">
-            <article className="skills__details">
-            <BsFillPatchCheckFill className="skills__details-icon" />
-              <p>Java</p>
-            </article>
-            <article className="skills__details">
-            <BsFillPatchCheckFill className="skills__details-icon" />
-              <p>Springboot</p>
-            </article>            
-            <article className="skills__details">
-            <BsFillPatchCheckFill className="skills__details-icon" />
-              <p>Javascript</p>
-            </article>                       
-            <article className="skills__details">
-            <BsFillPatchCheckFill className="skills__details-icon" />
-              <p>APIs</p>
-            </article>  
-            <article className="skills__details">
-            <BsFillPatchCheckFill className="skills__details-icon" />
-              <p>Python</p>
-            </article>
-            <article className="skills__details">
-            <BsFillPatchCheckFill className="skills__details-icon" />
-              <p>HTML</p>
-            </article>
-            <article className="skills__details"> 
-            <BsFillPatchCheckFill className="skills__details-icon" />
-              <p>C/C++</p>
-            </article>
-            <article className="skills__details">
-            <BsFillPatchCheckFill className="skills__details-icon" />
-              <p>CSS</p>
-            </article>
-            <article className="skills__details">
-            <BsFillPatchCheckFill className="skills__details-icon" />
-              <p>SQL</p>
-            </article>            
-            <article className="skills__details">
-            <BsFillPatchCheckFill className="skills__details-icon" />
-              <p>React JS</p>
-            </article> 
-
+            {skill(skillsData.skills)}
           </div>
         </div>
-        <div className="skills__tools"> 
+        <div className="skills__tools">
           <h3>Tools</h3>
           <div className="skills__content">
-            <article className="skills__details">
-            <BsFillPatchCheckFill className="skills__details-icon" />
-              <p>Eclipse</p>
-            </article>
-            <article className="skills__details">
-            <BsFillPatchCheckFill className="skills__details-icon" />
-              <p>IntelliJ IDEA</p>
-            </article>
-            <article className="skills__details">
-            <BsFillPatchCheckFill className="skills__details-icon" />
-              <p>VS Code</p>
-            </article>            
-            <article className="skills__details">
-            <BsFillPatchCheckFill className="skills__details-icon" />
-              <p>Postman</p>
-            </article>
-            <article className="skills__details">
-            <BsFillPatchCheckFill className="skills__details-icon" />
-              <p>Git & GitHub</p>
-            </article>          
-            <article className="skills__details">
-            <BsFillPatchCheckFill className="skills__details-icon" />
-              <p>Docker</p>
-            </article>
-            <article className="skills__details">
-            <BsFillPatchCheckFill className="skills__details-icon" />
-              <p>Linux</p>
-            </article>       
-            <article className="skills__details">
-            <BsFillPatchCheckFill className="skills__details-icon" />
-              <p>Vim</p>
-            </article>   
-            <article className="skills__details">
-            <BsFillPatchCheckFill className="skills__details-icon" />
-              <p>Bash</p>
-            </article>       
-            <article className="skills__details">
-            <BsFillPatchCheckFill className="skills__details-icon" />
-              <p>Android Studio</p>
-            </article>       
+            {skill(skillsData.tools)}
           </div>
-        </div>        
+        </div>
       </div>
     </section>
   );
-}
+};
 
 export default Skills;
