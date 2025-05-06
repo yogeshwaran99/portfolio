0b970bcca8e2d6c4dad047c29d6074e76ee37a93
yogeshwaran99 <itsyogeshwaran@gmail.com>
2025-05-06 23:02:22 +0530

    css style

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -7,12 +7,13 @@ const Contact = () => {
   return (
     <section id="contact">
       <h5>Get In Touch</h5>
-      <h2>Contact Me</h2>
+      <h2>Contact Me</h2>    
       <div className="container contact__container">       
           <article className="contact__option">
             <a href="mailto:itsyogeshwaran@gmail.com" ><MdOutlineEmail className='contact__option-icon'/>itsyogeshwaran@gmail.com</a>
           </article>
         </div>      
+        
     </section>
   );
 };
